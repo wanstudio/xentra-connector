937dece6b81e30895f36ed0422008d9193ddc16f
wanstudio <99557053+wanstudio@users.noreply.github.com>
2026-09-10 07:17:55 +0700

    feat: add connector capability registry

---
+++ src/capabilities/registry.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { contractVersion } = require('../config');
+
+const CAPABILITIES = Object.freeze({
+  health: true,
+  readiness: true,
+  capability_discovery: true,
+  contract_negotiation: true,
+  typed_persistence: false,
+  object_storage: false,
+});
+
+function getCapabilities() {
+  return Object.freeze({
+    contract_versions: [contractVersion],
+    capabilities: CAPABILITIES,
+  });
+}
+
+module.exports = { getCapabilities };
